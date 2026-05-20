daf8416459bdcde0351a2f75ede0010ad3cc2979
jyothi kumari <radhikaa0077@gmail.com>
2026-05-20 07:02:59 +0530

    Add files via upload

---
+++ Allterm.java
@@ -0,0 +1,19 @@
+public class Allterms
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a=99, d=-4;
+for(int i=1; i<=n; i++)
+{
+System.out.println(a+" ");
+a+=d;
+}
+}}
+
+
+
+
+
+
